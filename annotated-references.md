b9d6d7872cc4668b13aed3663299d0dadd197601
zahraii <zahraii@chapman.edu>
2023-09-30 16:09:36 -0700

    add select and update sources

--- annotated-references.md
+++ annotated-references.md
@@ -4,3 +4,9 @@
     - This is the most important reference as it's the basics for how we would code the rest of our project
 - [BlocklyLambdaCalculus](https://alexhkurz.github.io/BlocklyLambdaCalculus/)
     - We can use this as a reference to understand the JavaScript code we would need
+- [Unicode](https://www.compart.com/en/unicode/search?q=space#characters)
+    - Used this to find the blank unicode character
+- [W3 School](https://www.w3schools.com/sql/default.asp)
+    - Use this to reference what SQL keywords we need 
+- [HTML Color Codes](https://htmlcolorcodes.com/)
+    - Use to find color codes for blocks 
--- design-blocks/custom_blocks.js
+++ design-blocks/custom_blocks.js
@@ -1,142 +1,58 @@
 Blockly.HSV_SATURATION = 1;
 
-Blockly.Blocks['abs'] = {
+// SELECT
+Blockly.Blocks['select'] = {
   init: function() {
-    this.appendValueInput("VAR")
-        .setCheck("var")
-        .appendField("λ");
-    this.appendValueInput("EXPR")
-        .setCheck(["var", "exp", "Number"])
-        .appendField(".");
-    this.setInputsInline(true);
-    this.setOutput(true, "exp");
-    this.setColour(130);
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }
-};
-
-Blockly.Blocks['app'] = {
-  init: function() {
-    this.appendValueInput("EXPR1")
-        .setCheck(["var", "exp", "Number"])
-    this.appendValueInput("EXPR2")
-        .setCheck(["var", "exp", "Number"]);
-    this.setInputsInline(true);
-    this.setOutput(true, "exp");
-    this.setColour(330);
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }
-};
-
-Blockly.Blocks['var'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("")
-        .appendField(new Blockly.FieldTextInput("x"), "NAME");
-    this.setOutput(true, "var");
-    this.setColour(430);
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }
-};
-
-Blockly.Blocks['math_number'] = {
-  /**
-   * Block for numeric value.
-   * @this {Blockly.Block}
-   */
-  init: function() {
-    this.setHelpUrl(Blockly.Msg.MATH_NUMBER_HELPURL);
-    this.setColour(Blockly.Msg.MATH_HUE);
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldNumber('3'), 'NUM');
-    this.setOutput(true, 'Number');
-    this.setTooltip(Blockly.Msg.MATH_NUMBER_TOOLTIP);
-  }
-};
-
-Blockly.JavaScript.forBlock['abs'] = function(block) {
-  var var_name = Blockly.JavaScript.valueToCode(block, 'VAR', Blockly.JavaScript.ORDER_NONE);
-  var expr_code = Blockly.JavaScript.valueToCode(block, 'EXPR', Blockly.JavaScript.ORDER_NONE);
-  var code = '(' + var_name + ' => ' + expr_code + ')';
-  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
-};
-
-Blockly.JavaScript.forBlock['app'] = function(block) {
-  var expr1_code = Blockly.JavaScript.valueToCode(block, 'EXPR1', Blockly.JavaScript.ORDER_NONE);
-  var expr2_code = Blockly.JavaScript.valueToCode(block, 'EXPR2', Blockly.JavaScript.ORDER_NONE);
-  var code = '(' + expr1_code + ')(' + expr2_code + ')';
-  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
-};
-
-Blockly.JavaScript.forBlock['var'] = function(block) {
-  var var_name = block.getFieldValue('NAME');
-  var code = var_name;
-  return [code, Blockly.JavaScript.ORDER_ATOMIC];
-};
-
-Blockly.Blocks['math_arithmetic'] = {
-  /**
-   * Block for arithmetic operations.
-   * @this {Blockly.Block}
-   */
-  init: function() {
-    this.setHelpUrl(Blockly.Msg.MATH_ARITHMETIC_HELPURL);
-    this.setColour(Blockly.Msg.MATH_HUE);
-    this.setOutput(true, 'Number');
-    this.appendValueInput('A')
-        .setCheck(['Number', 'var', 'exp']);
-    this.appendDummyInput()
+    this.appendValueInput("SELECT")
+        .appendField('SELECT ')
+        .setCheck(['input','as','all','membership','agg_min','agg_max','agg_avg','agg_count','agg_sum'])
         .appendField(new Blockly.FieldDropdown([
-            ['+', 'ADD'],
-            ['-', 'MINUS'],
-            ['\u00D7', 'MULTIPLY'],
-            ['\u00F7', 'DIVIDE'],
-            ['^', 'POWER']]), 'OP');
-    this.appendValueInput('B')
-        .setCheck(['Number', 'var', 'exp']);
-    this.setInputsInline(true);
-    this.setTooltip(Blockly.Msg.MATH_ARITHMETIC_TOOLTIP);
+          ['\u0020', 'blank'],
+          ['ALL', 'all']
+          ['DISTINCT', 'distinct']]), 'op');
+    this.setInputsInline(false);
+    this.setPreviousStatement(true); //add these blocks when created -- do we even need this?
+    this.setNextStatement(true); //add when created
+    this.setColour('#53DC9E');
+
   }
 };
 
-Blockly.Blocks['functionDef'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldTextInput("f"), "NAME")
-        .appendField("=");
-    this.appendValueInput("EXPR")
-        .setCheck(["var", "exp", "Number"]);
-    this.setInputsInline(true);
-    this.setOutput(true, "exp");
-    this.setColour(48);
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }
-};
+// USER INPUT 
 
-Blockly.JavaScript['functionDef'] = function(block) {
-  var func_name = block.getFieldValue('NAME');
-  var expr_code = Blockly.JavaScript.valueToCode(block, 'EXPR', Blockly.JavaScript.ORDER_NONE);
-  var code = 'const ' + func_name + ' = ' + expr_code + ';';
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
+// AS 
 
-Blockly.Blocks['functionCall'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldTextInput("f"), "NAME");
-    this.setOutput(true, "exp");
-    this.setColour(48);
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }
-};
+// ALL 
 
-Blockly.JavaScript['functionCall'] = function(block) {
-  var func_name = block.getFieldValue('NAME');
-  var code = func_name;
-  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
-};
+// MEMBERSHIP OPERATOR (DOT NOTATION)
+
+// AGGREGATES (MIN, MAX, AVG, COUNT, SUM)
+
+//---------------------------
+
+// FROM
+
+// JOIN
+
+// WHERE 
+
+// GROUP BY 
+
+// HAVING 
+
+// ORDER BY 
+
+
+// and
+// between 
+// boolean 
+// compare
+// date 
+// in / not in 
+// limit 
+// math 
+// not 
+// null 
+// number 
+// or 
+// table 

9dc8fafe46331ff104d7419a894a76d139ef7358
Esha Yamani <yamani@chapman.edu>
2023-10-01 18:16:44 -0700

    small changes

--- design-blocks/custom_blocks.js
+++ design-blocks/custom_blocks.js
@@ -5,7 +5,7 @@ Blockly.Blocks['select'] = {
   init: function() {
     this.appendValueInput('SELECT')
         .appendField('SELECT ')
-        .setCheck(['input','as','all','membership','agg_min','agg_max','agg_avg','agg_count','agg_sum'])
+        //.setCheck(['input','as','all','membership','agg_min','agg_max','agg_avg','agg_count','agg_sum'])
         .appendField(new Blockly.FieldDropdown([
           ['\u0020', 'blank'],
           ['ALL', 'all']
@@ -33,7 +33,7 @@ Blockly.Blocks['from'] = {
   init: function() {
     this.appendValueInput('FROM')
         .appendField('FROM ')
-        .setCheck(['table', 'as'])
+        //.setCheck(['table', 'as'])
     this.setInputsInline(false); 
     this.setPreviousStatement(true, ['SELECT']); 
     this.setNextStatement(true, ['WHERE']); 
@@ -52,10 +52,10 @@ Blockly.Blocks['from'] = {
 Blockly.Blocks['where'] = {
   init: function() {
     this.appendValueInput('WHERE')
-        .setCheck(['input', 'and', 'or', 'compare', 'null', 'between', 'in', 'not'])
+        //.setCheck(['input', 'and', 'or', 'compare', 'null', 'between', 'in', 'not'])
         .appendField('WHERE ')
     this.setPreviousStatement(true, ['FROM']); 
-    this.setNextStatement(true, ['GROUP BY'])
+    //this.setNextStatement(true, ['GROUP BY'])
     this.setColour('#53DC9E');
     this.setTooltip('Your WHERE statement');
   }
@@ -88,3 +88,11 @@ Blockly.Blocks['where'] = {
 // date 
 // math 
 // number 
+
+
+Blockly.JavaScript.forBlock['select'] = function(block) {
+  var var_name = Blockly.JavaScript.valueToCode(block, 'VAR', Blockly.JavaScript.ORDER_NONE);
+  var expr_code = Blockly.JavaScript.valueToCode(block, 'EXPR', Blockly.JavaScript.ORDER_NONE);
+  var code = '(' + var_name + ' => ' + expr_code + ')';
+  return [code, Blockly.JavaScript.ORDER_FUNCTION_CALL];
+};

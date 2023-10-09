02dc77a24842621d813e03af0f73831429e8f43a
Esha Yamani <yamani@chapman.edu>
2023-10-09 12:29:48 -0700

    add comments

--- design-blocks/custom_blocks.js
+++ design-blocks/custom_blocks.js
@@ -1,7 +1,8 @@
-// SELECT - 'select'
+// SELECT and FROM 'select+from'
+// first block of a query
 Blockly.Blocks['SELECT+FROM'] = {
   init: function() {
-
+    // SELECT statement with dropdown menu
     this.appendValueInput('SELECT')
       .appendField('SELECT ')
         //.setCheck(['input','as','all','membership','agg_min','agg_max','agg_avg','agg_count','agg_sum'])
@@ -10,9 +11,10 @@ Blockly.Blocks['SELECT+FROM'] = {
         ['ALL', '*'],
         ['DISTINCT', 'distinct']]), 'SELECT_FIELD');
     
+    // FROM statement with dropdown menu of table names
     this.appendValueInput('FROM')
       .appendField('FROM ')
-        //.setCheck(['table', 'as'])
+      // adding 5 out of 17 table names 
       .appendField(new Blockly.FieldDropdown([
         ['\u0020', 'blank'],
         ['ACTOR', 'actor'],
@@ -22,26 +24,27 @@ Blockly.Blocks['SELECT+FROM'] = {
         ['COUNTRY', 'country']]), 'FROM_FIELD');
 
     this.setInputsInline(false);
-    this.setPreviousStatement(false); // would this be true?
-    this.setNextStatement(true, null); //add when created
-    this.setOutput(true) //for recursive purposes
-    this.setColour('#89CFF0');
+    this.setPreviousStatement(false); // no previoud
+    this.setNextStatement(true, null); // add when created
+    this.setOutput(true) // for recursive purposes (subqueries)
+    this.setColour('#89CFF0'); // light blue
     this.setTooltip('Your SELECT and FROM statement');
   }
 };
 
+// generate code block for SELECT+FROM
 Blockly.JavaScript['SELECT+FROM'] = function(block) {
+  // select dropdown
   var select = block.getFieldValue('SELECT_FIELD');
-  //var select = Blockly.JavaScript.valueToCode(block, 'SELECT', Blockly.JavaScript.ORDER_NONE);
+  // from dropdown
   var from = block.getFieldValue('FROM_FIELD');
-  //var from = block.JavaScript.getFieldValue(block, 'FROM', Blockly.JavaScript.ORDER_NONE);
-  
-  // Ensure that the generated code is properly formatted
-  var code = 'SELECT ' + select + ' FROM ' + from;
+
+  var code = 'SELECT ' + select + text + ' FROM ' + from;
   return code;
 };
 
 // USER INPUT - 'input'
+// displayed as a text block for any purpose
 Blockly.Blocks['INPUT'] = {
   init: function() {
     this.appendValueInput('INPUT')
@@ -54,11 +57,15 @@ Blockly.Blocks['INPUT'] = {
   }
 };
 
+// generate code for input block
 Blockly.JavaScript['INPUT'] = function(block) {
+  // given text 
   var input = block.getFieldValue('USER_INPUT');
   return [input, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+// NUMBER - 'number'
+// displayed as a number for any purpose such as limit
 Blockly.Blocks['NUMBER'] = {
   init: function() {
     this.appendDummyInput()
@@ -69,7 +76,9 @@ Blockly.Blocks['NUMBER'] = {
   }
 };
 
+// generate code for number block
 Blockly.JavaScript['NUMBER'] = function(block) {
+  // given number
   var numValue = block.getFieldValue('NUM');
   return [numValue, Blockly.JavaScript.ORDER_ATOMIC];
 };
@@ -88,6 +97,8 @@ Blockly.JavaScript['NUMBER'] = function(block) {
 // JOIN - 'join' 
 
 // COMPARISON - 'compare'
+// takes in two inputs as text or numbers with a operator between them
+// used for blocks like WHERE
 Blockly.Blocks['COMPARE'] = {
   /**
    * Block for arithmetic operations.
@@ -97,8 +108,10 @@ Blockly.Blocks['COMPARE'] = {
     this.setHelpUrl(Blockly.Msg.MATH_ARITHMETIC_HELPURL);
     this.setColour(Blockly.Msg.MATH_HUE);
     this.setOutput(true, 'Number');
+    // first value
     this.appendValueInput('A')
         .setCheck(['Number', 'var', 'exp']);
+    // adding dropdown menu of operators required
     this.appendDummyInput()
         .appendField(new Blockly.FieldDropdown([
             ['=', '='],
@@ -106,6 +119,7 @@ Blockly.Blocks['COMPARE'] = {
             ['<', '<'],
             ['>=', '>='],
             ['<=', '<=']]), 'compare');
+    // second value
     this.appendValueInput('B')
         .setCheck(['Number', 'var', 'exp']);
     this.setInputsInline(true);
@@ -114,12 +128,15 @@ Blockly.Blocks['COMPARE'] = {
   }
 };
 
+// generate code for compare block
 Blockly.JavaScript['COMPARE'] = function(block) {
+  // take in first value
   var A = Blockly.JavaScript.valueToCode(block, 'A', Blockly.JavaScript.ORDER_ATOMIC);
+  // take in second value
   var B = Blockly.JavaScript.valueToCode(block, 'B', Blockly.JavaScript.ORDER_ATOMIC);
+  // get opertaor for dropdown
   var operator = block.getFieldValue('compare')
 
-  // Ensure that the generated code is properly formatted
   var code = A + ' ' + operator + ' ' + B;
   return code;
 };
@@ -128,8 +145,8 @@ Blockly.JavaScript['COMPARE'] = function(block) {
 Blockly.Blocks['WHERE'] = {
   init: function() {
     this.appendValueInput('WHERE')
-        //.setCheck(['input', 'and', 'or', 'compare', 'null', 'between', 'in', 'not'])
         .appendField('WHERE ');
+    // can be connected anywhere to anything
     this.setPreviousStatement(true, null); 
     this.setNextStatement(true, null);
     this.setColour('#CDB7F6');
@@ -137,9 +154,11 @@ Blockly.Blocks['WHERE'] = {
   }
 }; 
 
+// generate code for where block
 Blockly.JavaScript['WHERE'] = function(block) {
+  // from compare block connected to where
   var where = Blockly.JavaScript.statementToCode(block, 'WHERE', Blockly.JavaScript.ORDER_NONE);
-  // Ensure that the generated code is properly formatted
+  
   var code = ' WHERE ' + where;
   return code;
 };
@@ -167,6 +186,7 @@ Blockly.Blocks['LIMIT'] = {
   init: function() {
     this.appendValueInput('LIMIT')
         .appendField('LIMIT')
+        // check for number input
         .setCheck('Number');
     this.setInputsInline(true);
     this.setPreviousStatement(true, null); 
@@ -177,9 +197,11 @@ Blockly.Blocks['LIMIT'] = {
   }
 };
 
+// generate code for limit block
 Blockly.JavaScript['LIMIT'] = function(block) {
+  // get value of input such as a number
   var limit = Blockly.JavaScript.valueToCode(block, 'LIMIT', Blockly.JavaScript.ORDER_ATOMIC);
-  // Ensure that the generated code is properly formatted
+
   var code = ' LIMIT ' + limit;
   return code;
 };
@@ -199,11 +221,3 @@ Blockly.JavaScript['LIMIT'] = function(block) {
 //   code = ';'
 //   return code;
 // }
-
-
-
-
-// boolean 
-// date 
-// math 
-// number 

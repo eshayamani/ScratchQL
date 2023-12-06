d323d53feed576a9e4876b7250928499983f885e
Esha Yamani <yamani@chapman.edu>
2023-12-05 18:09:17 -0800

    convert to python

--- src/custom_blocks.js
+++ src/custom_blocks.js
@@ -36,18 +36,18 @@ Blockly.Blocks['SELECT+FROM'] = {
 };
 
 // generate code block for SELECT+FROM
-Blockly.JavaScript['SELECT+FROM'] = function(block) {
+Blockly.Python['SELECT+FROM'] = function(block) {
   // select dropdown
   var select = block.getFieldValue('SELECT_FIELD');
   // add in line select text
   var text = (block.getFieldValue('SELECT_TEXT'));
   // add any text from input blocks
-  var input = Blockly.JavaScript.statementToCode(block, 'SELECT', Blockly.JavaScript.ORDER_ATOMIC);
+  var input = Blockly.Python.statementToCode(block, 'SELECT', Blockly.Python.ORDER_ATOMIC);
   // from dropdown
   var from = block.getFieldValue('FROM_FIELD');
 
   var code = 'SELECT ' + select + text + input + ' FROM ' + from;
-  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+  return [code, Blockly.Python.ORDER_ATOMIC];
 };
 
 // USER INPUT - 'input'
@@ -65,10 +65,10 @@ Blockly.Blocks['INPUT'] = {
 };
 
 // generate code for input block
-Blockly.JavaScript['INPUT'] = function(block) {
+Blockly.Python['INPUT'] = function(block) {
   // given text 
   var input = block.getFieldValue('USER_INPUT');
-  return [input, Blockly.JavaScript.ORDER_ATOMIC];
+  return [input, Blockly.Python.ORDER_ATOMIC];
 };
 
 // NUMBER - 'number'
@@ -84,10 +84,10 @@ Blockly.Blocks['NUMBER'] = {
 };
 
 // generate code for number block
-Blockly.JavaScript['NUMBER'] = function(block) {
+Blockly.Python['NUMBER'] = function(block) {
   // given number
   var numValue = block.getFieldValue('NUM');
-  return [numValue, Blockly.JavaScript.ORDER_ATOMIC];
+  return [numValue, Blockly.Python.ORDER_ATOMIC];
 };
 
 // AGGREGATES - 'aggregate'
@@ -112,11 +112,11 @@ Blockly.Blocks['AGGREGATE'] = {
 };
 
 // generate code block for AGGREGATE
-Blockly.JavaScript['AGGREGATE'] = function(block) {
+Blockly.Python['AGGREGATE'] = function(block) {
   // select dropdown
   var agg = block.getFieldValue('AGG');
   // get text from input block
-  var input = Blockly.JavaScript.valueToCode(block, 'AGGREGATE', Blockly.JavaScript.ORDER_ATOMIC);
+  var input = Blockly.Python.valueToCode(block, 'AGGREGATE', Blockly.Python.ORDER_ATOMIC);
   // format for queries
   var code = agg + '(' + input + ')';
   return code;
@@ -156,11 +156,11 @@ Blockly.Blocks['COMPARE'] = {
 };
 
 // generate code for compare block
-Blockly.JavaScript['COMPARE'] = function(block) {
+Blockly.Python['COMPARE'] = function(block) {
   // take in first value
-  var A = Blockly.JavaScript.valueToCode(block, 'A', Blockly.JavaScript.ORDER_ATOMIC);
+  var A = Blockly.Python.valueToCode(block, 'A', Blockly.Python.ORDER_ATOMIC);
   // take in second value
-  var B = Blockly.JavaScript.valueToCode(block, 'B', Blockly.JavaScript.ORDER_ATOMIC);
+  var B = Blockly.Python.valueToCode(block, 'B', Blockly.Python.ORDER_ATOMIC);
   // get opertaor for dropdown
   var operator = block.getFieldValue('compare')
 
@@ -183,18 +183,18 @@ Blockly.Blocks['WHERE'] = {
 }; 
 
 // generate code for where block
-Blockly.JavaScript['WHERE'] = function(block) {
+Blockly.Python['WHERE'] = function(block) {
   // get type of connected block
   var connectedBlock = block.getInputTargetBlock('WHERE');
   // check if its for compare
   if (connectedBlock && connectedBlock.type === 'COMPARE') {
     // use statement to code
-    var where = Blockly.JavaScript.statementToCode(block, 'WHERE', Blockly.JavaScript.ORDER_NONE);
+    var where = Blockly.Python.statementToCode(block, 'WHERE', Blockly.Python.ORDER_NONE);
     var code = ' WHERE ' + where;
   } // check if its for input
   else if (connectedBlock.type === 'INPUT') {
     // use value to code
-    var where = Blockly.JavaScript.valueToCode(block, 'WHERE', Blockly.JavaScript.ORDER_NONE);
+    var where = Blockly.Python.valueToCode(block, 'WHERE', Blockly.Python.ORDER_NONE);
     var code = ' WHERE ' + where;
   }
   else { // default case
@@ -221,9 +221,9 @@ Blockly.Blocks['GROUPBY'] = {
 };
 
 // generate code for group by block
-Blockly.JavaScript['GROUPBY'] = function(block) {
+Blockly.Python['GROUPBY'] = function(block) {
   // get value of group by variable
-  var group = Blockly.JavaScript.valueToCode(block, 'GROUPBY', Blockly.JavaScript.ORDER_ATOMIC);
+  var group = Blockly.Python.valueToCode(block, 'GROUPBY', Blockly.Python.ORDER_ATOMIC);
   var code = ' GROUP BY ' + group;
   return code;
 };
@@ -252,9 +252,9 @@ Blockly.Blocks['ORDERBY'] = {
 };
 
 // generate code for group by block
-Blockly.JavaScript['ORDERBY'] = function(block) {
+Blockly.Python['ORDERBY'] = function(block) {
   // get value of order by variable
-  var group = Blockly.JavaScript.valueToCode(block, 'ORDERBY', Blockly.JavaScript.ORDER_ATOMIC);
+  var group = Blockly.Python.valueToCode(block, 'ORDERBY', Blockly.Python.ORDER_ATOMIC);
   // get asc or desc value
   var order = block.getFieldValue('order')
   var code = ' ORDER BY ' + group + ' ' + order;
@@ -278,9 +278,9 @@ Blockly.Blocks['LIMIT'] = {
 };
 
 // generate code for limit block
-Blockly.JavaScript['LIMIT'] = function(block) {
+Blockly.Python['LIMIT'] = function(block) {
   // get value of input such as a number
-  var limit = Blockly.JavaScript.valueToCode(block, 'LIMIT', Blockly.JavaScript.ORDER_ATOMIC);
+  var limit = Blockly.Python.valueToCode(block, 'LIMIT', Blockly.Python.ORDER_ATOMIC);
   var code = ' LIMIT ' + limit;
   return code;
 };
@@ -296,32 +296,43 @@ Blockly.Blocks['CONNECTION'] = {
   }
 };
 
-// Generate JavaScript code for the SQLite connection block
-Blockly.JavaScript['CONNECTION'] = function(block) { 
-  //var query = Blockly.JavaScript.valueToCode(block, 'SELECT+FROM', Blockly.JavaScript.ORDER_ATOMIC);
+// Generate Python code for the SQLite connection block
+Blockly.Python['CONNECTION'] = function(block) { 
     // Get the connected block's code
-  var connectedCode = Blockly.JavaScript.valueToCode(block, 'CONNECTION', Blockly.JavaScript.ORDER_ATOMIC);
+  var connectedCode = Blockly.Python.valueToCode(block, 'CONNECTION', Blockly.Python.ORDER_ATOMIC);
 
   // Generate the connection code
   var code = `
-    var mysql = require('mysql');
-
-    var cnx = mysql.createConnection({
-        host: "34.27.128.43",
-        user: "root",
-        password: "pl",
-        database: "sakila"
-    });
-
-    cnx.connect(function(err) {
-        if (err) throw err;
-        var sql = "${connectedCode}"
-        cnx.query(sql, function (err, result, fields) {
-            if (err) throw err;
-            cnx.destroy(); 
-        });
-    });
-  `;
+  # Replace the following information with your database credentials
+  db_config = {
+      'host': '34.27.128.43',
+      'user': 'root',
+      'password': 'pl',
+      'database': 'sakila'
+  }
+  
+  # Establish a connection to the MySQL server
+  cnx = mysql.connector.connect(db_config)
+  
+  try:
+      cursor = cnx.cursor()
+  
+      # Replace 'your_table' with the actual table name
+      query = "${connectedCode}"
+      
+      cursor.execute(query)
+  
+      # Fetch all the rows
+      result = cursor.fetchall()
+  
+      for row in result:
+          print(row)
+  
+  finally:
+      # Close the cursor and connection
+      cursor.close()
+      cnx.close()`  
+  ;
 
   return code;
 };
